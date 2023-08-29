30b01b8a63d2b3a8b535254048f40168e76c74ea
Shalon Teoh <shalonteoh@gmail.com>
2023-08-29 10:57:59 +0800

    chore(core): Rename custom folders to core

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CountriesCustomConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'countries_custom'
+    name = 'core'
--- travel_backend/settings.py
+++ travel_backend/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'countries',
-    'countries_custom',
     'tags',
     'likes',
+    'core',
 ]
 
 MIDDLEWARE = [
